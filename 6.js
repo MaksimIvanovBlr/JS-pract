4f97f070978097822254343f42f54bea24246c6a
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2023-06-04 18:25:18 +0300

    1-10. Quite simple

---
+++ 6.js
@@ -0,0 +1,31 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+
+const someString = 'string'
+const someInt = 10
+const someBoolean = true
+const someVarible1 = null
+let someVarible2
+const someArr = []
+const someObj = {}
+
+console.log(typeof someString);
+console.log(typeof someInt);
+console.log(typeof someBoolean);
+console.log(typeof someVarible1);
+console.log(typeof someVarible2);
+console.log(typeof someArr);
+console.log(typeof someObj);
